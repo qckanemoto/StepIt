978454f09163f05045dd0fb5fd2fae92732396f6
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2015-02-21 14:51:21 +0900

    add Content entity

---
+++ src/Stepit/Bundle/AppBundle/Entity/Content.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Stepit\Bundle\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Content
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Stepit\Bundle\AppBundle\Entity\ContentRepository")
+ */
+class Content
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="value", type="string", length=255)
+     */
+    private $value;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     * @return Content
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string 
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
